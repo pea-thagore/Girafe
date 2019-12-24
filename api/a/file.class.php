281b92d0f0681025dc3720a0bebee2d8ea8c47d3
BARDON Nathan <nbardon@lazyfox.fr>
2019-12-24 16:08:00 +0100

    Adding future methods.

--- api/a/file.class.php
+++ api/a/file.class.php
@@ -2,11 +2,14 @@
 class file {
 
 
-	public static function create() {
-		echo "je cree";
-	}
-
 	public static function delete() {
 		echo "je suppr";
 	}
+	public static function rename() {
+	}
+	public static function properties() {
+	}	
+	public static function content() {
+	}
+		
 }
